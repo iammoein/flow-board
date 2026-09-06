c52f4a8b48c4482f51364ee9cab9128be82bb25e
Moein Moradi <moein.morradi@gmail.com>
2026-09-06 19:55:49 +0330

    feat: add icons to routes

--- src/constants/routes.contant.js
+++ src/constants/routes.contant.js
@@ -1,3 +1,6 @@
+import DashboardIcon from "@/components/icons/dashboard.icon.vue";
+import IssuesIcon from "@/components/icons/issues.icon.vue";
+
 export const ROUTE_NAMES = {
   DASHBOARD: "dashboard",
   ISSUES: "issues",
@@ -7,3 +10,8 @@ export const ROUTE_PATHS = {
   DASHBOARD: "/",
   ISSUES: "/issues",
 };
+
+export const NAV_ITEMS = [
+  { label: "داشبورد", name: ROUTE_NAMES.DASHBOARD, icon: DashboardIcon },
+  { label: "مسائل", name: ROUTE_NAMES.ISSUES, icon: IssuesIcon },
+];

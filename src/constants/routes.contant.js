9f1a1d7312320db895a410af1acbc7356372aded
Moein Moradi <moein.morradi@gmail.com>
2026-09-06 19:41:39 +0330

    feat: add routes for pages

---
+++ src/constants/routes.contant.js
@@ -0,0 +1,9 @@
+export const ROUTE_NAMES = {
+  DASHBOARD: "dashboard",
+  ISSUES: "issues",
+};
+
+export const ROUTE_PATHS = {
+  DASHBOARD: "/",
+  ISSUES: "/issues",
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import App from '@/App.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { ROUTE_NAMES, ROUTE_PATHS } from "@/constants/routes.contant";
+
+import Dashboard from "@/views/dashboard.view.vue";
+import Issues from "@/views/issues.view.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: App,
+      path: ROUTE_PATHS.DASHBOARD,
+      name: ROUTE_NAMES.DASHBOARD,
+      component: Dashboard,
     },
-  
+    {
+      path: ROUTE_PATHS.ISSUES,
+      name: ROUTE_NAMES.ISSUES,
+      component: Issues,
+    },
   ],
-})
+});
 
-export default router
+export default router;

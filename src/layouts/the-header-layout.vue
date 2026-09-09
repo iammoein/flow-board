10f2a34f72c223477d20b18c6b4087bd0c5646e2
Moein Moradi <moein.morradi@gmail.com>
2026-09-09 18:02:42 +0330

    feat: complete the header component

--- src/layouts/the-header-layout.vue
+++ src/layouts/the-header-layout.vue
@@ -5,13 +5,22 @@
     </div>
     <div class="header__actions">
       <SearchBar v-model="input" class="header__actions-search" />
+      <base-button variant="primary" class="header__actions-button">
+        <BaseIcon :icon="PlusIcon" :size="9" />
+        New
+      </base-button>
     </div>
   </header>
 </template>
 
 <script setup>
-import SearchBar from "@/components/shared/search-bar.component.vue";
 import { ref } from "vue";
+
+import BaseButton from "@/components/base/base-button.component.vue";
+import BaseIcon from "@/components/base/base-icon.component.vue";
+import SearchBar from "@/components/shared/search-bar.component.vue";
+
+import PlusIcon from "@/components/icons/plus.icon.vue";
 
 defineProps({
   headerTitle: {
@@ -41,9 +50,21 @@ const input = ref("");
     font-weight: 500;
   }
 
+  &__actions {
+    @include flex($align:center);
+    gap: space(4);
+  }
+
   &__actions-search {
     width: rem(220);
     height: rem(36);
+  }
+
+  &__actions-button {
+    @include flex($justify: center, $align: center);
+    gap: space(1.5);
+
+    font-size: rem(12);
   }
 }
 </style>

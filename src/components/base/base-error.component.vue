c70a4c2e514e8b210f2d6bb20bff9e061f5a4e04
Moein Moradi <moein.morradi@gmail.com>
2026-09-12 19:23:07 +0330

    feat: add base error component

---
+++ src/components/base/base-error.component.vue
@@ -0,0 +1,37 @@
+<template>
+  <Transition name="error">
+    <span v-if="message" class="base-error">
+      {{ message }}
+    </span>
+  </Transition>
+</template>
+
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    defautl: "",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.base-error {
+  display: block;
+
+  font-size: rem(12);
+
+  color: $error;
+}
+
+.error-enter-active,
+.error-leave-active {
+  transition: all 0.2s ease;
+}
+
+.error-enter-from,
+.error-leave-to {
+  opacity: 0;
+  transform: translateY(-4px);
+}
+</style>

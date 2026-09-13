99d57246f048a9e6185ddff8e67fea3645d6ad48
Moein Moradi <moein.morradi@gmail.com>
2026-09-13 19:35:56 +0330

    feat: implement store for creating issue

--- src/stores/issues.store.js
+++ src/stores/issues.store.js
@@ -1,13 +1,23 @@
+import { formatDate } from "@/utils/formatDate.util";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useIssuesStore = defineStore('issues', () => {
+export const useIssuesStore = defineStore("issues", () => {
+  const issues = ref([]);
 
   const isCreateIssuesModal = ref(false);
 
+  const createIssue = (issue = {}) => {
+    issues.value.push({
+      ...issue,
+      id: Date.now(),
+      date: formatDate(new Date()),
+    });
+  };
 
   return {
+    issues,
     isCreateIssuesModal,
-  }
-
-})
+    createIssue,
+  };
+});

1d5a0798ff37490b10a9d18bd9a2a6b3ab719ad6
Moein Moradi <moein.morradi@gmail.com>
2026-09-13 19:30:24 +0330

    chore: install pinia persist for save data in localStorage

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./assets/styles/main.scss";
 
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");

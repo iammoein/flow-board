6a01ad3bfa6298f4e568a63ebd2b7ae9e5d5ea13
Moein Moradi <moein.morradi@gmail.com>
2026-09-15 18:48:18 +0330

    feat: add different variants for issueLabels

--- src/components/Issues/issue-labels.compoent.vue
+++ src/components/Issues/issue-labels.compoent.vue
@@ -1,6 +1,10 @@
 <template>
   <ul class="issue-labels">
-    <li v-for="label in issueLabels" class="issue-labels__label">
+    <li
+      v-for="label in issueLabels"
+      class="issue-labels__label"
+      :class="`issue-labels__label--${labelSize}`"
+    >
       {{ label }}
     </li>
   </ul>
@@ -12,6 +16,11 @@ defineProps({
     type: Array,
     defult: () => [],
   },
+  labelSize: {
+    type: String,
+    default: "sm",
+    validator: (v) => ["sm", "md", "lg"].includes(v),
+  },
 });
 </script>
 
@@ -20,14 +29,28 @@ defineProps({
   @include flex($wrap: wrap);
   gap: space(1);
 
-  font-size: rem(10);
   font-weight: 500;
 
   &__label {
-    padding: space(0.5) space(1.5);
-
     border-radius: $radius-sm;
     background-color: $neutral-surface;
+
+    &--sm {
+      padding: space(0.5) space(1.5);
+
+      font-size: rem(10);
+    }
+
+    &--md {
+      padding: space(1) space(2);
+
+      font-size: rem(11);
+      font-weight: 600;
+    }
+
+    &--lg {
+      padding: space(1.5) space(2.5);
+    }
   }
 }
 </style>

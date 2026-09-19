ca9c2e8ecab406bb4446394ff82b4a1a1380ab93
Moein Moradi <moein.morradi@gmail.com>
2026-09-19 20:58:35 +0330

    chore: configure ESLint with Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
-import eslintPluginVue from "eslint-plugin-vue";
+import eslintPluginVue from 'eslint-plugin-vue';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  ...eslintPluginVue.configs["flat/recommended"],
+  ...eslintPluginVue.configs['flat/recommended'],
+
   {
     rules: {
-      "vue/attributes-order": "error",
+      'vue/attributes-order': 'error',
     },
   },
+
+  eslintConfigPrettier,
 ];

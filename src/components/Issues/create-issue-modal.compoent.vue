4255803febece8909e024e31f30952dea8248428
Moein Moradi <moein.morradi@gmail.com>
2026-09-20 18:10:36 +0330

    refactor: use kebab-case convention for component

--- src/components/Issues/create-issue-modal.compoent.vue
+++ src/components/Issues/create-issue-modal.compoent.vue
@@ -1,27 +1,31 @@
 <template>
-  <BaseModal v-model="isCreateIssuesModal" class="issue">
+  <base-modal
+    v-model="isCreateIssuesModal"
+    teleport-to="#app-body"
+    class="issue"
+  >
     <div class="issue__header">
       <h3 class="issue__title">{{ modalTitle }}</h3>
       <CloseButton class="issue__close-button" @close="handleCloseModal" />
     </div>
     <TheDivider class="issue__divider" />
 
-    <form @submit.prevent="handleSubmit" class="issue__form">
+    <form class="issue__form" @submit.prevent="handleSubmit">
       <div>
         <BaseInput
+          id="issue-title"
           v-model="formData.title"
           label="Title"
           placeholder="Add a title for your issue..."
-          id="issue-title"
         />
         <BaseError :message="error?.title?.[0]" />
       </div>
       <div>
         <BaseInput
+          id="issue-description"
           v-model="formData.description"
           as="textarea"
           label="Description"
-          id="issue-description"
           placeholder="Add a description or outline technical notes..."
         />
         <BaseError :message="error?.description?.[0]" />
@@ -47,35 +51,35 @@
         <base-button type="submit">Create Issue</base-button>
       </div>
     </form>
-  </BaseModal>
+  </base-modal>
 </template>
 
 <script setup>
-import z from "zod";
-import { reactive, ref } from "vue";
-import { storeToRefs } from "pinia";
-import { useIssuesStore } from "@/stores/issues.store.js";
-import { issuesSchema } from "@/schemas/issues.schema.js";
+import z from 'zod';
+import { reactive, ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useIssuesStore } from '@/stores/issues.store.js';
+import { issuesSchema } from '@/schemas/issues.schema.js';
 
-import BaseInput from "../base/base-input.component.vue";
-import BaseModal from "../base/base-modal.component.vue";
-import BaseDropdown from "../base/‌base-dropdown.component.vue";
-import BaseButton from "../base/base-button.component.vue";
-import BaseError from "../base/base-error.component.vue";
-import TheDivider from "../shared/the-divider.component.vue";
+import BaseInput from '../base/base-input.component.vue';
+import BaseModal from '../base/base-modal.component.vue';
+import BaseDropdown from '../base/‌base-dropdown.component.vue';
+import BaseButton from '../base/base-button.component.vue';
+import BaseError from '../base/base-error.component.vue';
+import TheDivider from '../shared/the-divider.component.vue';
 
-import CloseButton from "../shared/close-button.component.vue";
+import CloseButton from '../shared/close-button.component.vue';
 
 defineProps({
   modalTitle: {
     type: String,
-    defualt: "Create Issue",
+    defualt: 'Create Issue',
   },
 });
 
 const formData = reactive({
-  title: "",
-  description: "",
+  title: '',
+  description: '',
 });
 
 const error = ref({});
@@ -106,8 +110,8 @@ const handleSubmit = () => {
 
 const resetForm = () => {
   Object.assign(formData, {
-    title: "",
-    description: "",
+    title: '',
+    description: '',
   });
 };
 </script>
